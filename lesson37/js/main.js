4e037517499eae60779ed29767c994bd1a3ff03c
Lumi-09 <hazirilum12@gmail.com>
2025-10-20 17:41:19 +0200

    lesson38
    lesson38

--- lesson37/js/main.js
+++ lesson37/js/main.js
@@ -2,3 +2,17 @@ $('#sort').sortable();
 $(function() {
     $('#accordion').accordion();
 });
+
+$('#Tabs').tabs();
+
+$(function(){
+    $("#accordion").accordion({
+        collapsible: true
+    });
+});
+
+$(function(){
+    $("#Tabs").tabs({
+        collapsible: true
+    });
+});
